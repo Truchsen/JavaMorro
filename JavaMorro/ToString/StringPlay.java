cbde96ba33df619483159f7c88ff7f83f27a504d
vsoyseth <vsoyseth@gmail.com>
2014-10-13 13:29:57 +0200

    Playing around with toString() method in java

--- JavaMorro/ToString/StringPlay.java
+++ JavaMorro/ToString/StringPlay.java
@@ -7,13 +7,14 @@
 
 class StringPlay{
 	public static void main(String[] args) {
-		SomeWord w = new SomeWord("Some words");
+		SomeWord w = new SomeWord("Example");
 
 		/*This works because the  toString() method is awesome!
 		 *Even tough w is the type SomeWord, it will return a string in this context
 		 */
 		System.out.prinln(w);
-		/*The problem however is ife you want to use methods in the String class.
+
+		/*The problem however is if you want to use methods in the String class.
 		 *You cannot call for example w.charAt(1), because its not a method in SomeWOrd.
 		 *However you can call on the method toString() as underneath...
 		 */
@@ -23,11 +24,11 @@ class StringPlay{
 
 class SomeWord{
 	private String word;
-
+	
 	public SomeWord(String word){
 		this.word=word;
 	}
-
+	
 	public String toString(){
 		return word;
 	}
